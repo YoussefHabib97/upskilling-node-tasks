f7a1189bf5389be3679d172dee589671ea47c478
Youssef Habib <ykmhabib97@gmail.com>
2026-02-23 14:45:26 +0200

    Init commit - Session 1 task done

---
+++ Session 1/index.js
@@ -0,0 +1,6 @@
+
+let userName = window.prompt("What is your name?")
+let age = window.prompt("What is your age?")
+
+document.getElementById("name").innerHTML = userName;
+document.getElementById("age").innerHTML = age;
